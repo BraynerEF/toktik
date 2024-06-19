be1895496287af5230bde05ad209f3fc927ddee5
BraynerEF <147545983+BraynerEF@users.noreply.github.com>
2024-06-18 23:17:41 -0400

    Add files via upload

---
+++ lib/domain/entities/video_post.dart
@@ -0,0 +1,20 @@
+
+class VideoPost {
+
+  final String caption;
+  final String videoUrl;
+  final int likes;
+  final int views;
+  final int comments;
+
+  VideoPost({
+    required this.caption,
+    required this.videoUrl,
+    this.likes = 0,
+    this.views = 0,
+    this.comments = 0,
+  });
+
+ 
+
+}
---
+++ lib/shared/data/local_video_posts.dart
@@ -0,0 +1,63 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Subiendo escaleras automáticas',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 23230,
+    'views': 1523,
+    'comments': 25,
+    
+  },
+  {
+    'name': 'Mi mar Azul',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 24230,
+    'views': 1343,
+    'comments': 25,
+  },
+  {
+    'name': 'Vacaciones Check jajaj😜',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 21564320,
+    'views': 123563,
+    'comments': 7115,
+  },
+  {
+    'name': 'Hermosas vistas',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 320,
+    'views': 2300,
+    'comments': 925,
+  },
+  {
+    'name': 'Fondo de pantalla',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 3230,
+    'views': 31030,
+    'comments': 725,
+  },
+  {
+    'name': 'Un Tomioka para no perder la costumbre',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 1000,
+    'views': 330,
+    'comments': 125,
+  },
+  {
+    'name': 'Un poquito de anime',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 1320,
+    'views': 33032,
+    'comments': 250,
+  },
+  {
+    'name': '#Goku #Sayayin',
+    'videoUrl': 'assets/videos/8.mp4',
+    'likes': 3420000,
+    'views': 3332,
+    'comments': 30,
+  },
+
+
+];
+
+
